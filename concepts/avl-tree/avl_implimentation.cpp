9c329a2b13377ea6637be065bc3fe24055e6a888
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2020-10-26 16:36:11 +0530

    change almost whole code to by value and make delete function

--- concepts/avl-tree/avl_implimentation.cpp
+++ concepts/avl-tree/avl_implimentation.cpp
@@ -41,88 +41,138 @@ int balancefactor(node* root){
     int hr=root->right?root->right->ht:0;
     return hl-hr; 
 }
-void LLrotation(node** root){
-    bug();
-    // node *temp2=*root;
-    node* p=(*root)->left;
+node* LLrotation(node* root){
+    node* p=root->left;
     node* temp=p->right;
-    p->right=*root;
-    (*root)->left=temp;
-    (*root)->ht=height(*root);
+    p->right=root;
+    root->left=temp;
+    root->ht=height(root);
     p->ht=height(p);
-    *root=p;
-    return;
+    return p;
 }
-void LRrotation(node** root){
-    node *temp=(*root)->left;
+node* LRrotation(node* root){
+    node *temp=(root)->left;
     node* temp2=temp->right;
     temp->right=temp2->left;
-    (*root)->left=temp2->right;
+    (root)->left=temp2->right;
     temp2->left=temp;
-    temp2->right=*root;
+    temp2->right=root;
     temp2->ht=height(temp2);
     temp->ht=height(temp);
-    (*root)->ht=height(*root);
-    *root=temp2;
-    return;
+    (root)->ht=height(root);
+    return temp2;
 }
-void RRrotation(node** root){
-    node* temp=(*root)->right;
+node* RRrotation(node* root){
+    node* temp=(root)->right;
     node* temp2=temp->left;
-    temp->left=*root;
-    (*root)->right=temp2;
+    temp->left=root;
+    (root)->right=temp2;
     temp->ht=height(temp);
-    (*root)->ht=height(*root);
-    *root=temp;
-    return;
+    (root)->ht=height(root);
+    return temp;
 }
-void RLrotation(node** root){
-    node* temp=(*root)->right;
+node* RLrotation(node* root){
+    node* temp=(root)->right;
     node* temp2=temp->left;
-    (*root)->right=temp2->left;
+    (root)->right=temp2->left;
     temp->left=temp2->right;
-    temp2->left=*root;
+    temp2->left=root;
     temp2->right=temp;
     temp->ht=height(temp);
     temp2->ht=height(temp2);
-    (*root)->ht=height(*root);
-    *root=temp2;
-    return;
+    (root)->ht=height(root);
+    return temp2;
 }
-void insert(node** root,int x){
+node* insert(node* root,int x){
     node* temp=new node(x);
-    if(*root==NULL){
-        *root=temp;
-        return;
+    if(root==NULL){
+        root=temp;
+        return root;
     }
-    else if((*root)->data>x) insert(&((*root)->left),x);
-    else insert(&((*root)->right),x);
-    (*root)->ht=height(*root);
-    if(balancefactor(*root)>1&&x<(*root)->left->data){
-        bug();
-        LLrotation(&(*root));
-        bug();
+    else if((root)->data>x) root->left=insert(((root)->left),x);
+    else root->right=insert(((root)->right),x);
+    (root)->ht=height(root);
+    if(balancefactor(root)>1&&x<(root)->left->data){
+        return LLrotation((root));
     }
-    else if(balancefactor(*root)>1&&x>(*root)->left->data){
-        LRrotation(&(*root));
+    else if(balancefactor(root)>1&&x>(root)->left->data){
+        return LRrotation(root);
     }
-    else if(balancefactor(*root)<-1&&x>(*root)->right->data){
-        RRrotation(&(*root));
+    else if(balancefactor(root)<-1&&x>(root)->right->data){
+        return RRrotation((root));
     }
-    else if(balancefactor(*root)<-1&&x<(*root)->right->data){
-        RLrotation(&(*root));
+    else if(balancefactor(root)<-1&&x<(root)->right->data){
+        return RLrotation((root));
     }
+    return root;
 }
-
+node* minval(node* root){
+    node* curr=root;
+    if(root==NULL) return NULL;
+    while(curr&&curr->left!=NULL) curr=curr->left;
+    return curr;
+}
+node* avldel(node* root,int x){
+    if(root==NULL) return root;
+    else if((root)->data>x) root->left=avldel(root->left,x);
+    else if(root->data<x) root->right=avldel(root->right,x);
+    else{
+        if(root->right==NULL&&(root)->left==NULL){
+            node* temp=root;
+            delete temp;
+            root==NULL;
+        }
+        else if((root)->left==NULL){
+            node* t=(root);
+            (root)=(root)->right;
+            delete t;
+        }
+        else if((root)->right==NULL){
+            node* t=(root);
+            (root)=(root)->left;
+            delete t;
+        }
+        else{
+            node* t=minval(root->right);
+            (root)->data=t->data;
+            root->right=avldel(root->right,t->data);
+        }
+    }
+    if(root==NULL) return root;
+    root->ht=height(root);
+    int bal=balancefactor(root);
+    if(bal>1 && balancefactor(root->left)>=0){
+        return LLrotation((root));
+    }
+    else if(bal>1 && balancefactor(root->left)<0){
+        return LRrotation((root));
+    }
+    else if(bal<-1 && balancefactor(root->right)<=0){
+        bug();
+        return RRrotation((root));
+    }
+    else if(bal<-1 && balancefactor(root->right)>0){
+        bug();
+        return RLrotation((root));
+    }
+    return root;
+}
 int main(){
     // ios_base::sync_with_stdio(false);
 	// cin.tie(NULL);
-    node* head=NULL;
-    insert(&head,5);
-    // cout<<head->data<<"\n";
-    insert(&head,7);
-    // cout<<head->data<<"\n";
-    insert(&head,6);
-    preorder(head);
+    node* root=NULL;
+    root = insert(root, 9);  
+    root = insert(root, 5);  
+    root = insert(root, 10);  
+    root = insert(root, 0);  
+    root = insert(root, 6);  
+    root = insert(root, 11);  
+    root = insert(root, -1);  
+    root = insert(root, 1);  
+    root = insert(root, 2); 
+    preorder(root);
+    root=avldel(root,10);
+    cout<<endl;
+    preorder(root);
     return 0;
 }
