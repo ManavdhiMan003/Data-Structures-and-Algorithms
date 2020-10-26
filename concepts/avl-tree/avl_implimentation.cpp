e2707d67bab6c5000b2731a601b57d647079d813
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2020-10-26 10:12:41 +0530

    LL rotation

---
+++ concepts/avl-tree/avl_implimentation.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define loop(i,n) for(i=0;i<n;i++)
+#define loop1(i,k,n) for(i=k;i<n;i++)
+#define pb push_back
+#define bug() 1;//cout<<__LINE__<<endl;
+typedef long long int ll;
+struct node{
+    int data;
+    node* left;
+    node* right;
+    int ht;
+    node(int x){
+        data=x;
+        left=right=NULL;
+        ht=1;
+    }
+};
+void inorder(node* root){
+    if(root==NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    return;
+}
+void preorder(node* root){
+    if(root==NULL) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+    return;
+}
+int height(node* root){
+    if(root==NULL) return 0;
+    return max(height(root->left),height(root->right))+1;
+}
+int balancefactor(node* root){
+    if(root==NULL) return 0;
+    int hl=root->left?root->left->ht:0;
+    int hr=root->right?root->right->ht:0;
+    return hl-hr; 
+}
+void LLrotation(node** root){
+    bug();
+    // node *temp2=*root;
+    node* p=(*root)->left;
+    node* temp=p->right;
+    p->right=*root;
+    (*root)->left=temp;
+    (*root)->ht=height(*root);
+    p->ht=height(p);
+    *root=p;
+    return;
+}
+void insert(node** root,int x){
+    node* temp=new node(x);
+    if(*root==NULL){
+        *root=temp;
+        return;
+    }
+    else if((*root)->data>x) insert(&((*root)->left),x);
+    else insert(&((*root)->right),x);
+    (*root)->ht=height(*root);
+    if(balancefactor(*root)>1&&x<(*root)->left->data){
+        bug();
+        LLrotation(&(*root));
+        bug();
+    }
+    // else if(balancefactor(*root)==2&&balancefactor((*root)->left)==-1){
+    //     LRrotation(&(*root));
+    // }
+    // else if(balancefactor(*root)==-2&&balancefactor((*root)->right)==1){
+    //     RRrotation(&(*root));
+    // }
+    // else if(balancefactor(*root)==-2&&balancefactor((*root)->right)==-1){
+    //     RLrotation(&(*root));
+    // }
+}
+
+int main(){
+    // ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
+    node* head=NULL;
+    insert(&head,5);
+    // cout<<head->data<<"\n";
+    insert(&head,4);
+    // cout<<head->data<<"\n";
+    insert(&head,3);
+    preorder(head);
+    return 0;
+}
