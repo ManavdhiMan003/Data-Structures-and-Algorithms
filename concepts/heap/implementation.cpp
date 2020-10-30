752c8ed4368420e1669a4effd7e38b0caee01283
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2020-10-30 23:18:49 +0530

    heapify

--- concepts/heap/implementation.cpp
+++ concepts/heap/implementation.cpp
@@ -32,12 +32,38 @@ int del(int a[],int n){
     a[n]=temp;
     return temp;
 }
+void heapify(int a[],int n){
+    int s=n/2;
+    for(int i=s;i>0;i--){
+        int l=2*i,r=2*i+1,c=i,k=i;
+        while(l<n||r<n){
+            if(a[l]>a[c]&&r<n){
+                c=l;
+            }
+            if(a[r]>a[c]&&r<n){
+                c=r;
+            }
+            if(c!=k){
+                swap(a[c],a[k]);
+                k=c;
+            }
+            l=2*k;
+            r=2*k+1;
+            c=k;
+        }
+    }
+}
 int main(){
     int a[8]={0,10,50,75,95,32,56,78};
+    heapify(a,8);
+    for(int i=1;i<8;i++) cout<<a[i]<<" ";
+    cout<<endl;
     for(int i=2;i<8;i++){
         insert(a,i);
     }
+    for(int i=1;i<8;i++) cout<<a[i]<<" ";
+    cout<<endl;
     for(int i=7;i>0;i--) del(a,i);
     for(int i=1;i<8;i++) cout<<a[i]<<" ";
-    return 0;
+    return 0;   
 }
