ef09cffdce179d7dc8b8a59fd0159b77be0c529a
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2020-11-03 10:51:58 +0530

    BFS of graph

---
+++ concepts/graphs/dfs_bfs.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define loop(i,n) for(i=0;i<n;i++)
+#define loop1(i,k,n) for(i=k;i<n;i++)
+#define pb push_back
+typedef long long int ll;
+void BFS(int G[][7],int start,int n){
+    int visited[7]={0};
+    cout<<start<<" ";
+    visited[start]=1;
+    queue<int> q;
+    q.push(start);
+    int i=start;
+    while(!q.empty()){
+        i=q.front();
+        q.pop();
+        for(int j=1;j<n;j++){
+            if(G[i][j]==1&&visited[j]==0){ 
+                cout<<j<<" ";
+                visited[j]=1;
+                q.push(j);
+            }
+        }   
+    }
+    cout<<endl;
+}
+int main(){
+    int G[7][7]={{0,0,0,0,0,0,0},
+    {0,0,1,1,0,0,0},
+    {0,1,0,0,1,0,0},
+    {0,1,0,0,1,0,0},
+    {0,0,1,1,0,1,1},
+    {0,0,0,0,1,0,0},
+    {0,0,0,0,1,0,0}};
+    BFS(G,3,7);
+    return 0;
+}
